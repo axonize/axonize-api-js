2043d8f6e6dbed5213d7cb9d4606bf7f33cea4ed
Shaked Amar <shakeda@axonize.com>
2019-07-04 11:33:54 +0300

    0.1.0

--- src/__tests__/devices.test.ts
+++ src/__tests__/devices.test.ts
@@ -1,15 +1,14 @@
-import * as nock from "nock";
+import * as nock from 'nock';
 import Api from '../index';
 
 test('Test basic devices call', () => {
   nock('https://api.axonize.com')
-  .get('/odata/devices')
-  .reply(200, [{ name: 'Device 1' }]);
+    .get('/odata/devices')
+    .reply(200, [{ name: 'Device 1' }]);
 
   const api = new Api();
 
   return api.devices.getDevices().then(devices => {
     expect(devices[0].name).toBe('Device 1');
-  })
-  
+  });
 });
