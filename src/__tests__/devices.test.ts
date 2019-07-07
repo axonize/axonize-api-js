448827bf509866f3e1cb2ac6191c7d3021348d65
Shaked Amar <shakeda@axonize.com>
2019-07-07 11:33:10 +0300

    migrate the api UT to be integration test

--- src/__tests__/devices.test.ts
+++ src/__tests__/devices.test.ts
@@ -1,36 +1,22 @@
-import * as nock from 'nock';
 import Api from '../index';
 
-test('Test basic devices call', () => {
-  nock('https://api.axonize.com')
-    .get('/odata/devices')
-    .reply(200, { value: [{ name: 'Device 1' }] });
+const getCredentialsFromENV = () => {
+  return {
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret 
+  }
+}
 
-  const api = new Api();
+test('Test basic devices call', () => {
+  const api = new Api(getCredentialsFromENV());
 
   return api.devices.getDevices().then(devices => {
-    expect(devices[0].name).toBe('Device 1');
-  });
-});
-
-test('Test get single device entity', () => {
-  nock('https://api.axonize.com')
-    .get('/odata/devices/1')
-    .reply(200, { id: '1' });
-
-  const api = new Api();
-
-  return api.devices.getDevice('1').then(device => {
-    expect(device.id).toBe('1');
+    expect(!!devices.length).toEqual(true);
   });
 });
 
 test('Test device not found', async () => {
-  nock('https://api.axonize.com')
-    .get('/odata/devices/2')
-    .reply(404);
-
-  const api = new Api();
+  const api = new Api(getCredentialsFromENV());
 
   try {
     await api.devices.getDevice('2');
