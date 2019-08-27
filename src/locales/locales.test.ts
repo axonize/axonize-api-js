ab7db62aef61844717b194a5b114ca77fb7fce7d
Michael Salaverry <michaels@axonize.com>
2019-08-27 14:01:34 +0300

    refactor: add longer timeout

--- src/locales/locales.test.ts
+++ src/locales/locales.test.ts
@@ -1,6 +1,8 @@
 import Api from '../index';
 import { getCredentialsFromENV } from '../utils/tests';
 
+jest.setTimeout(parseInt(process.env.testTimeout || '60000'));
+
 test('Test getLocales', () => {
   const api = new Api(getCredentialsFromENV());
 
